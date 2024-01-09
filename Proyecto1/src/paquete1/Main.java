31162848d34588b23cdc17d7f54e67d808a1dbc0
borja <borja@DESKTOP-I657CM2>
2024-01-09 04:26:53 +0100

    segunda confirmasió

---
+++ Proyecto1/src/paquete1/Main.java
@@ -0,0 +1,11 @@
+package paquete1;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Ejemplo control de versiones con Git");
+		System.out.println("El rial segundo control de versionescon Git");
+	}
+
+}
